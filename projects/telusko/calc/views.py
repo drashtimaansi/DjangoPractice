18059c0de2b1190cf7de9ba8cde165318d03ce6b
drashtimaansi <mahdrashti1988@gmail.com>
2022-09-08 01:06:36 +0530

    Added result and form in home page for learning dynamic input entered by user

--- projects/telusko/calc/views.py
+++ projects/telusko/calc/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name':'Kasganj'})
+
+
+def add(request):
+
+    val1 = request.GET['num1']
+    val2 = request.GET['num2']
+    res = int(val1)+int(val2)
+    return render(request, "result.html", {'res':res})
